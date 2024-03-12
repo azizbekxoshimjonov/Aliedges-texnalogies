2e159f8889cf932a7f8dde5405d68bae5f35878a
azizbekxoshimjonov <azizbekxoshimjonov.veb@gmail.com>
2024-03-12 11:05:04 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,158 @@
+
+import './App.css';
+
+function App() {
+  return (
+    
+    <div >
+      <header>
+        <div className="container">
+          <nav className='Nav'>
+            <a href="#">
+              <img src="https://alliedge.in/images/AlliedgeLogo_SVG.svg" alt="" />
+              <h1 className='Ali'>Alliedge <br />
+                <span>Technologies</span>
+              </h1>
+              </a>
+            <ul className='Nav__list'>
+              <li className='Nav__item'><a href="#">Home</a> </li>
+              <li className='Nav__item'><a href="#">Services</a></li>
+              <li className='Nav__item'><a href="#">Technology</a></li>
+              <li className='Nav__item'><a href="#">Group Compnies</a></li>
+              <li className='Nav__item'><a href="#">About Us</a></li>
+            </ul>
+          </nav>
+        </div>
+      
+      <section>
+        <div className="container">
+          <h1 className='Title'>Expand your<br /> coverage with  <span> Alliedge.</span></h1>
+          <p className='des'>Customer satisfaction is at the heart of everything we do.We are committed to  providing exceptional IT services <br />
+                           that exceed your expectations and help you  achieve your business goals.</p>
+                           <button className='btn1'>Lets connect</button>
+        </div>
+      </section>
+      </header>
+      <section className='hero'>
+        <div className="container">
+          <h1 className='hero__title'>Our <span>Expertise</span></h1>
+          <div className='boxs'>
+            <div className='our1'>
+              <img src="https://media.licdn.com/dms/image/C4E0BAQGxinthzGQ7Xw/company-logo_200_200/0/1630568864735/allied_technologies_and_consulting_llc_logo?e=2147483647&v=beta&t=I-5Nc3mWttwI3L8z4kMeTMu9FoQRWfsiQdAlZyG_3ug" alt="" />
+              <h1>Quality Assurance</h1>
+              <p>Our commitment to <br />
+                quality enables us to <br />
+                deliver software solution <br />
+                that are only functional, <br />
+                but also user-friendly, <br />
+                reliable, secure, provides <br />
+                  expectational customer <br />
+                satisfaction</p>
+            </div>
+            <div className='our'>
+              <img src="https://media.licdn.com/dms/image/C4D0BAQHlscU7ToePOA/company-logo_200_200/0/1631342929904?e=2147483647&v=beta&t=WuyukEnbqzLHQFd7cVJvQo_Azq08k18lVe-f_JsUJJE" alt="" />
+              <h1>On time delivery</h1>
+              <p>Our commitment to <br />
+                quality enables us to <br />
+                deliver software solution <br />
+                that are only functional, <br />
+                but also user-friendly, <br />
+                reliable, secure, provides <br />
+                  expectational customer <br />
+                satisfaction</p>
+            </div>
+            <div className='our3'>
+              <img src="https://zenprospect-production.s3.amazonaws.com/uploads/pictures/62b05169f076b70001b1b268/picture" alt="" />
+              <h1>Advanced technical skilled team</h1>
+              <p>Our commitment to <br />
+                quality enables us to <br />
+                deliver software solution <br />
+                that are only functional, <br />
+                but also user-friendly, <br />
+                reliable, secure, provides <br />
+                  expectational customer <br />
+                satisfaction</p>
+            </div>
+            <div className='our4'>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFZg1nV-_btX-YY9jEqD2bZQ4mXuN8l6QPTQfCUK_UidRH1Bm46ojWFin8t2to3z3S5IE&usqp=CAU" alt="" />
+              <h1>Budget friendly</h1>
+              <p>Our commitment to <br />
+                quality enables us to <br />
+                deliver software solution <br />
+                that are only functional, <br />
+                but also user-friendly, <br />
+                reliable, secure, provides <br />
+                  expectational customer <br />
+                satisfaction</p>
+            </div>
+          </div>
+          <div className='pic'>
+            <img src="https://d8it4huxumps7.cloudfront.net/images/partners/new_organisation_image-7LdqFMuMtN15545801650IbuocIaSx.png" alt="" />
+          </div>
+          <div className='meet'>
+            <h1>Meet <span>Alliedge  </span></h1>
+            <div className='meet1'></div>
+          </div>
+          <div className='boks'>
+            <div className='boks1'>
+              <h1>Alliedge Tech</h1>
+             <h4> <a href="#">visit site</a></h4>
+              <h2>Software Based Company </h2>
+              <h3>Powered by Alliedge</h3>
+            </div>
+            <div className='boks1'>
+            <h1>Vision Softronics</h1>
+             <h4> <a href="#">visit site</a></h4>
+              <h2>Hardware Based Company </h2>
+              <h3>Powered by Alliedge</h3>
+            </div>
+          </div>
+          <div className='meet'>
+            <h1>Abaut <span>Us  </span></h1>
+            <div className='meet1'></div>
+          </div>
+          <div className='wee'>
+            <img src="https://www.alliedtechnologies.io/images/joomla.png" alt="" />
+            <div>
+              <h1>Who We Are</h1>           
+              <p>We are believers in change! A change is driven by technology and
+innovation. We help individuals, start-ups, businesses, and 
+enterprises adapt and adopt digital transformation. We aim to 
+change people's lives and improve businesses with our progressive,
+value-adding, innovative technology solutions.</p>
+            </div>
+          </div>
+          <div className='wee2'>
+          <h1>Who We Are</h1>           
+              <p>We are believers in change! A change is driven by technology and
+innovation. We help individuals, start-ups, businesses, and 
+enterprises adapt and adopt digital transformation. We aim to 
+change people's lives and improve businesses with our progressive,
+value-adding, innovative technology solutions.</p>
+<div>
+<img src="https://www.alliedtechnologies.io/images/joomla.png" alt="" />
+</div>
+          </div>
+          <div className='letsup'>
+            <h1>Let us plan your dream <br />
+project together.</h1>
+            <input type="text" placeholder='Name' name="" id="" />
+            <input type="text" placeholder='Eamil' name="" id="" />
+            <input type="text" placeholder='Mobile No' name="" id="" />
+            <input type="text" placeholder='Select Services' name="" id="" />
+            <input className='input1'  type="text" placeholder='Project Discription'  />
+            <button className='btn1'>Submit</button>
+
+</div>
+        
+       
+
+        </div>
+      </section>
+    
+    </div>
+
+  );
+}
+
+export default App;
